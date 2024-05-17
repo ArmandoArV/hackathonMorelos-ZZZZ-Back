cc24fcf16077196b592e063520a72445de68bc04
ArmandoArV <armando.arredondo.valle@gmail.com>
2024-05-17 02:25:55 -0600

    fixes afore router

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use("/", userRouter);
 app.use("/", investRouter);
-app.use("/api", aforeRoutes);
+app.use("/", aforeRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
