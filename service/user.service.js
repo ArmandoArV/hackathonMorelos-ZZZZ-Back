5aa8856d77bbc9d0078be7a74d313405c0c88b8a
ArmandoArV <armando.arredondo.valle@gmail.com>
2024-05-16 22:52:49 -0600

    basic user endpoints

--- service/user.service.js
+++ service/user.service.js
@@ -5,24 +5,16 @@ const userService = {
   validateUser: async (email, password) => {
     try {
       const [rows] = await connection.query(
-        "SELECT idUsers, Roles_idRole FROM Users WHERE email = ? AND password = SHA2(?,224)",
+        "SELECT idUsers FROM Users WHERE email = ? AND password = SHA2(?,224)",
         [email, password]
       );
 
       if (rows.length > 0) {
         const user = rows[0];
-        const isAdmin = user.Roles_idRole === 1;
-        const isBeecker = user.Roles_idRole === 2;
 
         let token;
-        if (isAdmin) {
-          token = jwtMiddleware.generateAdminToken(user.idUsers);
-        } else if (isBeecker) {
-          // token = jwtMiddleware.generateBeeckerToken(user.idUsers);
-          token = jwtMiddleware.generateToken(user.idUsers);
-        } else {
-          token = jwtMiddleware.generateToken(user.idUsers);
-        }
+
+        token = jwtMiddleware.generateToken(user.idUsers);
 
         return {
           token: token,
@@ -60,16 +52,6 @@ const userService = {
     }
   },
 
-  getUserTypes: async () => {
-    try {
-      const [rows] = await connection.query("SELECT * FROM Roles");
-
-      return rows;
-    } catch (error) {
-      throw error;
-    }
-  },
-
   getUserById: async (idUsers) => {
     try {
       const [rows] = await connection.query(
@@ -88,8 +70,8 @@ const userService = {
     password,
     name,
     lastName,
-    dateOfBirth,
-    userTypeId
+    secondLastName,
+    dateOfBirth
   ) => {
     try {
       // First check if the email already exists
@@ -105,28 +87,15 @@ const userService = {
 
       // If the email does not exist, proceed with creating the new user
       const sql =
-        "INSERT INTO Users (email, password, name, lastName, dateOfBirth, Roles_idRole) VALUES (?, SHA2(?,224), ?, ?, ?, ?)";
+        "INSERT INTO Users (email, password, name, lastName, secondLastName ,dateOfBirth ) VALUES (?, SHA2(?,224), ?, ?, ?, ?)";
       const [rows] = await connection.query(sql, [
         email,
         password,
         name,
         lastName,
+        secondLastName,
         dateOfBirth,
-        userTypeId,
       ]);
-
-      return rows;
-    } catch (error) {
-      throw error;
-    }
-  },
-
-  updateUserPermissions: async (idUsers, email, userTypeId) => {
-    try {
-      const [rows] = await connection.query(
-        "UPDATE Users SET email = ?, Roles_idRole = ? WHERE idUsers = ?",
-        [email, userTypeId, idUsers]
-      );
 
       return rows;
     } catch (error) {
