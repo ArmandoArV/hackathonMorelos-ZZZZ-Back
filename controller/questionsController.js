914693107ff2034d698dedb9e7d60613b3adec14
OsmarSanchez23 <osmaresanche23@gmail.com>
2024-05-17 19:30:49 -0600

    asd

--- controller/questionsController.js
+++ controller/questionsController.js
@@ -12,6 +12,58 @@ const questionsController = {
         } catch (error) {
           res.status(500).json({ message: error.message });
         }
+      },
+
+      getAforeQuestions: async (req, res) => {
+        try {
+          const objective = await questionsService.getAforeQuestions();
+          if (objective) {
+            res.status(200).json(objective);
+          } else {
+            res.status(404).json({ message: "Objective not found" });
+          }
+        } catch (error) {
+          res.status(500).json({ message: error.message });
+        }
+      },
+
+      getInversionQuestions: async (req, res) => {
+        try {
+          const objective = await questionsService.getInversionQuestions();
+          if (objective) {
+            res.status(200).json(objective);
+          } else {
+            res.status(404).json({ message: "Objective not found" });
+          }
+        } catch (error) {
+          res.status(500).json({ message: error.message });
+        }
+      },
+
+      getSaveQuestions: async (req, res) => {
+        try {
+          const objective = await questionsService.getSaveQuestions();
+          if (objective) {
+            res.status(200).json(objective);
+          } else {
+            res.status(404).json({ message: "Objective not found" });
+          }
+        } catch (error) {
+          res.status(500).json({ message: error.message });
+        }
+      },
+
+      getLearnFinanceQuestions: async (req, res) => {
+        try {
+          const objective = await questionsService.getInitialQuestions();
+          if (objective) {
+            res.status(200).json(objective);
+          } else {
+            res.status(404).json({ message: "Objective not found" });
+          }
+        } catch (error) {
+          res.status(500).json({ message: error.message });
+        }
       }
 }
 
--- service/questions.service.js
+++ service/questions.service.js
@@ -10,6 +10,50 @@ const questionsService = {
         } catch (error) {
           throw error;
         }
+      },
+
+      getAforeQuestions: async () => {
+        try {
+          const [rows] = await connection.query(
+            "SELECT * FROM hackathon.Questions WHERE typeQuestion = 1;",
+          );
+          return rows[0]; 
+        } catch (error) {
+          throw error;
+        }
+      },
+
+      getInversionQuestions: async () => {
+        try {
+          const [rows] = await connection.query(
+            "SELECT * FROM hackathon.Questions WHERE typeQuestion = 2;",
+          );
+          return rows[0]; 
+        } catch (error) {
+          throw error;
+        }
+      },
+
+      getSaveQuestions: async () => {
+        try {
+          const [rows] = await connection.query(
+            "SELECT * FROM hackathon.Questions WHERE typeQuestion = 3;",
+          );
+          return rows[0]; 
+        } catch (error) {
+          throw error;
+        }
+      },
+
+      getLearnFinanceQuestions: async () => {
+        try {
+          const [rows] = await connection.query(
+            "SELECT * FROM hackathon.Questions WHERE typeQuestion = 4;",
+          );
+          return rows[0]; 
+        } catch (error) {
+          throw error;
+        }
       }
 }
 
